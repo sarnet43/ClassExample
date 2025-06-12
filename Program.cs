0b1ff3b58388658e8c86828b5fbba58a7f79f847
lee sua <s2332@e-mirim.hs.kr>
2025-06-12 14:55:42 +0900

    메서드 오버라이딩 예

--- Program.cs
+++ Program.cs
@@ -12,29 +12,35 @@ namespace ClassExample
         class Parent
         {
             public int variable = 273;
+            public void Method() { Console.WriteLine("부모의 메서드"); }
+            public virtual void MethodO() { Console.WriteLine("부모의 메서드"); }
+
             public static int counter = 0;
-            public void Method() { Console.WriteLine("부모의 메소드"); }
-            public void CouterParent()
+
+            public void CounteParent()
             {
                 Parent.counter++;
             }
-            public Parent() {  Console.WriteLine("Parent()"); }
-            public Parent(int param) {  Console.WriteLine("Parent(int param)"); }
-            public Parent(string param) {  Console.WriteLine("Parent(string param)"); }
-           
+            public Parent() { Console.WriteLine("Parent()"); }
+            public Parent(int param) { Console.WriteLine("Parent(int param)"); }
+            public Parent(string param) { Console.WriteLine("Parent(string param)"); }
         }
 
         class Child : Parent
         {
             public new string variable = "하이딩";
-            public new void Method() { Console.WriteLine("자식의 메소드"); }
-            public void CouterChild()
+
+            public new void Method() { Console.WriteLine("자식의 메서드"); }
+
+            public override void MethodO() { Console.WriteLine("자식의 메서드"); }
+
+            public void CountChilde()
             {
                 Child.counter++;
             }
             public Child() : base(10)
             {
-                Console.WriteLine("Child() : base(10)");
+                Console.WriteLine("Child():base(10)");
             }
             public Child(string input) : base(input)
             {
@@ -42,21 +48,27 @@ namespace ClassExample
             }
         }
 
-        public static int number = 10; //가려진 부모 변수
+        public static int number = 10; // 가려진 부모 변수
+
         static void Main(string[] args)
         {
-            //섀도잉
-            int number = 20; //섀도잉 예, number 클래스 변수가 지연 변수로 가려짐
+
+            // 섀도잉
+            int number = 20;  // 섀도잉 예.number 클래스 변수가 지연 변수로 가려짐 
             Console.WriteLine(number);
             Console.WriteLine(Program.number);
 
             // 하이딩
             Child c = new Child();
             Console.WriteLine(c.variable);
-            Console.WriteLine(((Parent)c).variable); //하이딩 된 부모의 변수 접근
-            Console.WriteLine((c as Parent).variable); //하이딩 된 부모의 변수 접근
+            Console.WriteLine(((Parent)c).variable);  // 하이딩 된 부모의 변수 접근
+            Console.WriteLine((c as Parent).variable);// 하이딩 된 부모의 변수 접근
             c.Method();
-            ((Parent)c).Method(); //하이딩 된 부모의 메서드 접근
+            ((Parent)c).Method();// 하이딩 된 부모의 메서드 접근
+
+            // 오버라이딩
+            c.MethodO();
+            ((Parent)c).MethodO();// 오버라이딩 된 부모의 메서드 접근(자식 메서드 내용으로 덮어써짐)
 
             Child childA = new Child();
             Child childB = new Child("string");
@@ -64,10 +76,11 @@ namespace ClassExample
             Parent parent = new Parent();
             Child child = new Child();
 
-            parent.CouterParent();
-            child.CouterParent();
+            parent.CounteParent();
+            child.CounteParent();
             Console.WriteLine(Parent.counter);
             Console.WriteLine(Child.counter);
+
         }
     }
 }

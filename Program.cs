2f9de48165274df6a2ddeeba93017e7eea540e39
lee sua <s2332@e-mirim.hs.kr>
2025-06-12 14:46:21 +0900

    하이딩 예 (변수, 메서드)

--- Program.cs
+++ Program.cs
@@ -11,7 +11,9 @@ namespace ClassExample
     {
         class Parent
         {
+            public int variable = 273;
             public static int counter = 0;
+            public void Method() { Console.WriteLine("부모의 메소드"); }
             public void CouterParent()
             {
                 Parent.counter++;
@@ -24,6 +26,8 @@ namespace ClassExample
 
         class Child : Parent
         {
+            public string variable = "하이딩";
+            public void Method() { Console.WriteLine("자식의 메소드"); }
             public void CouterChild()
             {
                 Child.counter++;
@@ -41,9 +45,19 @@ namespace ClassExample
         public static int number = 10; //가려진 부모 변수
         static void Main(string[] args)
         {
+            //섀도잉
             int number = 20; //섀도잉 예, number 클래스 변수가 지연 변수로 가려짐
             Console.WriteLine(number);
             Console.WriteLine(Program.number);
+
+            // 하이딩
+            Child c = new Child();
+            Console.WriteLine(c.variable);
+            Console.WriteLine(((Parent)c).variable); //하이딩 된 부모의 변수 접근
+            Console.WriteLine((c as Parent).variable); //하이딩 된 부모의 변수 접근
+            c.Method();
+            ((Parent)c).Method(); //하이딩 된 부모의 메서드 접근
+
             Child childA = new Child();
             Child childB = new Child("string");
 
